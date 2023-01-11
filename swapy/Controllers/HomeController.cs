edfcf0d0ecc2074d40772c52a88b9c55c8509dd1
anwer ghanmi <ghanmianwer1@gmail.com>
2023-01-11 14:05:07 +0100

    landing page

--- swapy/Controllers/HomeController.cs
+++ swapy/Controllers/HomeController.cs
@@ -1,16 +1,20 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using swapy.Models;
+using Microsoft.AspNetCore.Http; //session4
+using Microsoft.AspNetCore.Identity; //password hashing
 
 namespace swapy.Controllers;
 
 public class HomeController : Controller
 {
+    private MyContext _context; //additional
     private readonly ILogger<HomeController> _logger;
 
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
+        // _context = context;
     }
 
     public IActionResult Index()
--- swapy/Program.cs
+++ swapy/Program.cs
@@ -1,7 +1,18 @@
+using Microsoft.EntityFrameworkCore; //additional library
+using swapy.Models;
 var builder = WebApplication.CreateBuilder(args);
+// Creates builder (also part of boilerplate code for web apps)
+//  Creates the db connection string
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddHttpContextAccessor(); //session1
+builder.Services.AddSession();  //session2
+builder.Services.AddDbContext<MyContext>(options =>
+{
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
 
 var app = builder.Build();
 
@@ -10,6 +21,7 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+app.UseSession();  //session3
 app.UseStaticFiles();
 
 app.UseRouting();
